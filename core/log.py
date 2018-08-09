f043955ec7e540b562830f1a9115c65b3b44e79f
liyao379178 <muzi379178228@qq.com>
2018-08-09 19:14:54 +0800

    Initial commit

---
+++ core/log.py
@@ -0,0 +1,13 @@
+
+# 基础包: 日志服务
+import logging
+
+def get_logger():
+    global logPath
+    try:
+        logPath
+    except NameError:
+        logPath = ""
+    FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=FORMAT)
+    return logging
